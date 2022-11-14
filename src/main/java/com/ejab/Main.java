3c32d2e00161ca8fea5b197c8d49812237791909
Eldar <9eldik7@gmail.com>
2022-11-14 16:48:43 +0300

    refactor - init file place

--- src/main/java/com/ejab/Main.java
+++ src/main/java/com/ejab/Main.java
@@ -1,3 +1,5 @@
+package com.ejab;
+
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/ejab/cource/community/prefix_sums/step1/A/PrefixSum.java
@@ -0,0 +1,4 @@
+package com.ejab.cource.community.prefix_sums.step1.A;
+
+public class PrefixSum {
+}
